b7ad5d371915d3ae9d359d7bcaa2bcd29874b93c
Ingolf Pernice <ingolfpernice@Carolynes-MBP.fritz.box>
2023-09-22 06:19:36 +0200

    Further adjustments for Quantlet bots

---
+++ osppaa_2021_draw_3dplots_wt/osppaa_2021_draw_3dplots_wt.R
@@ -0,0 +1,55 @@
+## >> >> >> >> >> >> >> >> >> >> >> >> >> >> >> >> >> >> >> >> >> >> #
+# >> PREP: Load data and packages                                    #
+## >> >> >> >> >> >> >> >> >> >> >> >> >> >> >> >> >> >> >> >> >> >> #
+source("yml_SETTINGS_loading.R")
+
+hgt <- 2.5
+wdt <- 2.5
+##############################################################
+
+trends_ST_LT <- function(st,dfv,mdl){
+  - 0.160*dfv + 0.085*dfv^2 - 0.417*dfv^3 - 0.266*st + 0.00*st^2 + 0.178*st^3
+}
+
+trend_st <- seq(-2,2,0.2)
+trend_lt <- seq(-2,2,0.2)
+return <- outer(X = trend_st
+                ,Y = trend_lt
+                ,trends_ST_LT)
+
+
+tikz(paste0(SETTINGS$modelling$resultspath,"3dplot_wT_mMCaV.tex")
+     , standAlone = FALSE
+     , width=wdt
+     , height=hgt)
+
+par(mgp=c(2,1,0), mar = c(0, 3, 0, 1))
+persp(x = trend_st
+      , y = trend_lt
+      , z = return
+      , xlab = "\n $T_{t}$"
+      , ylab = "\n $D_{t}$"
+      , zlab = "\n $\\widehat{\\Delta P}_{t+1}$"
+      , main = NULL
+      , sub = NULL
+      , theta = 120
+      , phi = 15
+      , r = sqrt(3)
+      , d = 1
+      , scale = TRUE
+      , expand = 1
+      , col = "white"
+      , border = NULL
+      , ltheta = -135
+      , lphi = 0
+      , shade = NA
+      , box = TRUE
+      , axes = TRUE
+      , nticks = 5
+      , cex.axis = 1.2
+      , ticktype = "detailed")
+
+
+dev.off()
+
+
